60ca5cbc3d6de874b8af2799699dece8461261a6
B0K1NG <129324719+B0K1NG@users.noreply.github.com>
2024-05-03 17:56:02 +0300

    Delete Easier directory

--- Easier/findMax.js
+++
@@ -1,5 +0,0 @@
-function findMax(arr) {
-    return Math.max.apply(null, arr)
-}
-
-console.log(findMax([3, 7, 1, 9, 4])); // Output: 9
--- Easier/longestWord.js
+++
@@ -1,6 +0,0 @@
-function findLongestWord(str) {
-    str = str.split(" ");
-    return str.sort((a,b) => b.length - a.length)[0]
-  }
-  
-  console.log(findLongestWord('This is a sample sentence'));
